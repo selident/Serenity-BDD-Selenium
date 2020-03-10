8648d7c52b1a4fd2c85f0266470d27f7b9da863a
Long Lee <hoanglongtc7@gmail.com>
2020-03-10 10:11:07 +0700

    Completed tests (#2)
    * completed two tests and changed some other configurations
    also support headless mode
    remove un-used libs
    
    * Update Readme.md (#1)
    
    * Update Readme.md
    
    * Update Readme.md
    
    Co-authored-by: Long Lee <longhoangly@gmail.com>

--- src/test/java/com/utest/test/SignUpTest.java
+++ src/test/java/com/utest/test/SignUpTest.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @RunWith(SerenityRunner.class)
@@ -18,15 +18,17 @@ public class SignUpTest {
     @Managed(uniqueSession = true)
     public WebDriver driver;
 
+    String LANDING_URL = "https://www.utest.com/";
+
     @Steps
     private SignUpSteps signUpSteps;
 
     @Test
     public void sign_up_successfully_test() {
 
-        List<String> languages = new ArrayList<String>(Arrays.asList("Russian"));
-        signUpSteps.go_to_sign_up_page("https://www.utest.com/");
-        signUpSteps.fill_info_for_first_sign_up_step("Long", "Lee", "longhoangly@gmail.com", "Male", "Jan", "20", "1990", languages);
+        List<String> languages = new ArrayList<String>(Collections.singletonList("Russian"));
+        signUpSteps.go_to_sign_up_page(LANDING_URL);
+        signUpSteps.fill_info_for_first_sign_up_step("Long", "Lee", "longhoangly@gmail.com", "Male", "January", "20", "1990", languages);
         signUpSteps.submit_step_one();
         signUpSteps.verify_if_sign_up_step_one_succeeded("Add your address");
     }
@@ -34,9 +36,9 @@ public class SignUpTest {
     @Test
     public void sign_up_failed_invalid_email_test() {
 
-        List<String> languages = new ArrayList<String>(Arrays.asList("Russian"));
-        signUpSteps.go_to_sign_up_page("https://www.utest.com/");
-        signUpSteps.fill_info_for_first_sign_up_step("Long", "Lee", "invalid_email", "Male", "Jan", "20", "1990", languages);
+        List<String> languages = new ArrayList<String>(Collections.singletonList("Russian"));
+        signUpSteps.go_to_sign_up_page(LANDING_URL);
+        signUpSteps.fill_info_for_first_sign_up_step("Long", "Lee", "invalid_email", "Male", "January", "20", "1990", languages);
         signUpSteps.submit_step_one();
         signUpSteps.verify_if_message_invalid_email_appear("Enter valid email");
     }
